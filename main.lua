b20d497c3ef36df2733f20f32d679cd625d5a3c9
Tony Pham <tony.phamx04@gmail.com>
2020-10-12 11:13:54 -0400

    Refactor main.lua to use functions and add variable and function scopes

--- main.lua
+++ main.lua
@@ -1,31 +1,45 @@
-cc = require("currencyConversion")
-choice = nil
-repeat
+local cc = require("currencyConversion")
+
+local function displayMainMenu()
     print("Main Menu")
     print("a. Convert Canadian Dollar to Japanese Yen")
     print("q. Quit")
     io.write("> ")
-    choice = io.read("l")
-    if choice == "a" or choice == "A" then
-        amt = nil
-        repeat
-            print("Converting the Canadian Dollar to Japanese Yen")
-            print("Enter the amount to convert.")
-            print("Press b to go back to the main menu.")
-            io.write("> ")
-            amt = io.read()
-            if tonumber(amt) then
-                amt = tonumber(amt)
-                if amt >= 0 then
-                    io.write(string.format("$%.2f is equal to %s yen.\n", amt, cc.convertCanadianDollarToJapaneseYen(amt)))
+end
+
+local choice = nil
+
+local function run()
+    repeat
+        displayMainMenu()
+        choice = io.read("l")
+        if choice == "a" or choice == "A" then
+            local amt = nil
+            repeat
+                print("Converting the Canadian Dollar to Japanese Yen")
+                print("Enter the amount to convert.")
+                print("Press b to go back to the main menu.")
+                io.write("> ")
+                amt = io.read()
+                if tonumber(amt) then
+                    amt = tonumber(amt)
+                    if amt >= 0 then
+                        io.write(string.format("$%.2f is equal to %s yen.\n", amt, cc.convertCanadianDollarToJapaneseYen(amt)))
+                    else
+                        io.write("A dollar amount cannot be negative. Please try again.")
+                    end
+                elseif amt == "b" or amt == "b" then
+                    io.write("Returning to the main menu..\n")
                 else
-                    io.write("A dollar amount cannot be negative. Please try again.")
+                    io.write("Invalid input. Please try again.\n")
                 end
-            elseif amt == "b" or amt == "b" then
-                io.write("Returning to the main menu..\n")
-            else
-                io.write("Invalid input. Please try again.\n")
-            end
-        until type(amt) == "number" or amt == "b" or amt == "B"
-    end
-until choice == "q" or choice == "Q"
+            until type(amt) == "number" or amt == "b" or amt == "B"
+        end
+    until choice == "q" or choice == "Q"
+end
+
+local function main()
+    run()
+end
+
+main()
